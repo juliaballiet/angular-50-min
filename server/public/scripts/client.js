86f92aa6eb4ae02b80c7d02035d69da30f379278
Julia Balliet <julia.m.balliet@gmail.com>
2018-08-08 13:38:14 -0500

    added button to log input field

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,5 +1,11 @@
 let FoodApp = angular.module('FoodApp', []);
 
 FoodApp.controller('FoodController', function(){
+    let vm = this;
+    vm.foods = [];
 
+    vm.addFood = function() {
+        vm.foods.push(vm.foodIn);
+        console.log(vm.foods);
+    }
 })
